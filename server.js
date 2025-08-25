1e42181a074244b5128081a99564411b9dc5bbcb
sagorsohel <sohel19sagor@gmail.com>
2025-08-26 00:53:21 +0600

    Refactor CORS configuration in server.js to allow all origins for improved flexibility

--- server.js
+++ server.js
@@ -17,15 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: function(origin, callback) {
-      if(!origin) return callback(null, true); // allow Postman, etc.
-      const regex = /^http:\/\/.*\.localhost:5174$/;
-      if(regex.test(origin) || origin === "http://localhost:5173") {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true, // allow all
     credentials: true
   }));
   
